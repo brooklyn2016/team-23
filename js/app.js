3bf4c61cc1d0a502e98e78e978ad0007ac90f27d
Elaine Burchman <elaine.sf.burchman@gmail.com>
2016-10-29 04:15:44 -0400

    updated homepage for admins.

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var myApp = angular.module('myApp', []); //arrat of dependencies
+
+
+//controller takes name and function
+myApp.controller(mainController, function() {  //reference within tag as ng-controller="mainController"
+
+
+
+});
